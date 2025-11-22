3da97a8497ab20f0f19bc15ba2615cf53bf65b22
Alan_Gutierrez <ala.gutierrezv@duocuc.cl>
2025-11-22 12:47:41 -0300

    cambios para fetch
    cambios para mejorar la conexion con la bd

--- Milsabores/Milsabores/src/main/java/com/pasteleria/Milsabores/Controller/UsuarioController.java
+++ Milsabores/Milsabores/src/main/java/com/pasteleria/Milsabores/Controller/UsuarioController.java
@@ -5,7 +5,9 @@ import com.pasteleria.Milsabores.Entity.Usuario;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
+// Si quisieras permitir cualquier origen en desarrollo, puedes usar:
+// @CrossOrigin(origins = "*")
 public class UsuarioController {
 
     @Autowired
